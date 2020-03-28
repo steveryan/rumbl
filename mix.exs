5f96eccf96bd8d4053515acde68fb485268c5f6a
steveryan <steveryan6989@gmail.com>
2020-03-28 14:29:33 -0400

    Adds password hashing.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Rumbl.MixProject do
   def application do
     [
       mod: {Rumbl.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :argon2_elixir, :comeonin]
     ]
   end
 
@@ -42,7 +42,8 @@ defmodule Rumbl.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:argon2_elixir, "~> 2.3"}
     ]
   end
 
