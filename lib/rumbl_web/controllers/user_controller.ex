5c5dbbe08e13f64658e93e1f9dd5a836548369b2
steveryan <steveryan6989@gmail.com>
2020-03-27 23:22:21 -0400

    Fixed Ecto chapter. Can create new users with /users/new

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,10 +1,31 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Repo
+  alias Rumbl.User
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} successfully created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
+  end
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render(conn, "index.html", users: users)
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
   end
 
   def show(conn, %{"id" => id}) do
