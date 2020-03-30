78113f8f23542403ebfb5dc34c386e74d022795b
steveryan <steveryan6989@gmail.com>
2020-03-29 23:03:20 -0400

    Adds some tests for video controller

---
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -0,0 +1,41 @@
+defmodule Rumbl.VideoControllerTest do
+  use RumblWeb.ConnCase
+
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(%{username: username})
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each(
+      [
+        get(conn, Routes.video_path(conn, :new)),
+        get(conn, Routes.video_path(conn, :index)),
+        get(conn, Routes.video_path(conn, :show, "123")),
+        get(conn, Routes.video_path(conn, :edit, "123")),
+        put(conn, Routes.video_path(conn, :update, "123", %{})),
+        post(conn, Routes.video_path(conn, :create, %{})),
+        delete(conn, Routes.video_path(conn, :delete, "123"))
+      ],
+      fn conn ->
+        assert html_response(conn, 302)
+        assert conn.halted
+      end
+    )
+  end
+
+  @tag login_as: "max"
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(%{username: "other"}), title: "another video")
+    conn = get(conn, Routes.video_path(conn, :index))
+    assert html_response(conn, 200) =~ "Listing Videos"
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
+end
